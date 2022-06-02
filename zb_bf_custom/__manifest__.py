befcbe571090fdbd4c14cad90108d7339781987c
Neha Jose <neha@zbeanztech.com>
2022-06-02 12:35:43 +0530

    [UPD][ZB-8790] Reference Person( Book unit- CRM)

--- zb_bf_custom/__manifest__.py
+++ zb_bf_custom/__manifest__.py
@@ -12,7 +12,7 @@
 
 
     """,
-    'version': '6.49',
+    'version': '6.50',
     'depends': ['base', 'account', 'snailmail_account', 'bh_account_vat', 'zb_crm_property', 'zb_building_management',
                 'purchase', 'crm', 'product','report_xlsx',],
     'data': [
